a608cc3e0edcf5b66da3047637c8fc97eaa4014e
jaau2422 <j.aumueller@web.de>
2023-10-24 18:33:42 +0100

    new commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import logo from './logo.svg';
+import Logo from './Bohowild42.png';
+import Logo2 from './Bohowild31.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <header className="App-header">
+      
+        <p>
+          SAVE THE DATE
+        </p>
+        <div className="p1" >
+        <p>
+          Ines & Martin
+        </p>
+        </div>
+        <p>
+          31.05.2024
+        </p>
+
+        <p>
+          ____________________
+        </p>
+        <div className="p2" >
+        <p>
+          Prüfeninger Schlossgarten Regensburg
+        </p>
+        <p>
+          Details folgen 
+        </p>
+        </div>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
