8de63a6d34b78108babb62dbf78691da3049d047
jaau2422 <j.aumueller@web.de>
2023-10-26 14:49:26 +0100

    changed iphone settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
+
 import logo from './logo.svg';
 import Logo from './Bohowild42.png';
 import Logo2 from './Bohowild31.png';
@@ -8,7 +9,14 @@ import PasswordInput from './components/Password';
 import ProtectedPage from './components/ProtectedPage';
 
 function App() {
-
+  useEffect(() => {
+    if (navigator.userAgent.indexOf('iPhone') > -1) {
+      const viewportMeta = document.querySelector("[name=viewport]");
+      if (viewportMeta) {
+        viewportMeta.setAttribute("content", "width=device-width, initial-scale=1, maximum-scale=1");
+      }
+    }
+  }, []);
   return (
     <div>
       <ProtectedPage />
