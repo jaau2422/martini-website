b31eccc92eb30cecc7ffda00f0d3a8571d74ef2c
jaau2422 <j.aumueller@web.de>
2024-05-30 22:54:51 +0100

    added seating plan

--- src/components/ProtectedPage.jsx
+++ src/components/ProtectedPage.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import PasswordInput from './Password';
 import { AddToCalendarButton } from 'add-to-calendar-button-react';
 
+import image from "./sitzplan.jpg"
+
 const ProtectedPage = () => {
   const [isPasswordRequired, setIsPasswordRequired] = useState(true);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -51,13 +53,13 @@ const ProtectedPage = () => {
         <p>
          Beginn: 15 Uhr
         </p>
-        <p>
-        Hier gehts zum Anmeldelink:
-        </p>
-        <button>
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfrbcsy-j2FM31-2ofX02U09NR_5y6BntHaYSbEvv68WpH2yg/viewform?usp=sf_link">  →  </a></button>
+        
+        <img className="responsive-image" src={image} alt="Description of image" />
+      
+        
         </div>
         
+          
       </header>
     </div>
       ) : null}
